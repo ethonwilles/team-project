a0b15f16c73e1897695f3161e3e593fe7912b70f
ethonwilles <ethonwilles@gmail.com>
2019-10-15 14:27:56 -0600

    asdfas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/home";
+import Profile from "./pages/profile";
 
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/profile" component={Profile} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "../styles/main.scss";
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+      </div>
+    );
+  }
+}
